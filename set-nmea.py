a9af0878b349238f72dda384d3084bc31518b0ab
Timo Juhani Lindfors <timo.lindfors@iki.fi>
2010-03-26 17:20:19 +0200

    Fix the script. It did not work at all before.

--- set-nmea.py
+++ set-nmea.py
@@ -19,12 +19,12 @@ def callback(ty, packet):
     if ty == "CFG-PRT":
         if sys.argv[1] == "on":
             # NMEA and UBX
-            packet[0]["In_proto_mask"] = 1 + 2
-            packet[0]["Out_proto_mask"] = 1 + 2
+            packet[1]["In_proto_mask"] = 1 + 2
+            packet[1]["Out_proto_mask"] = 1 + 2
         else:
             # only UBX
-            packet[0]["In_proto_mask"] = 1
-            packet[0]["Out_proto_mask"] = 1
+            packet[1]["In_proto_mask"] = 1
+            packet[1]["Out_proto_mask"] = 1
         t.send("CFG-PRT", 20, packet)
     elif ty == "ACK-ACK":
         loop.quit()
